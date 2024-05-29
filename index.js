d98bcab61099894f7c06d32d2b490231e32edf42
tanyanebhwani <92228566+tanyanebhwani@users.noreply.github.com>
2024-05-29 22:41:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ app.get('/', (req, res) => {
   res.send('News Proxy Server is running');
 });
 
-app.get('/proxyNewsApi/country/:country/category/:category/page/:page/pageSize/:pageSize', (req, res) => {
-  const { country , category , page , pageSize } = req.params;
+app.get('/proxyNewsApi/', (req, res) => {
+  console.log("hi");
+  /*const { country , category , page , pageSize } = req.params;
   const apiKey = process.env.NEWS_API_KEY;
   const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`;
   axios.get(url)
     .then(response => res.json(response.data))
-    .catch(error => res.status(500).send(error.message));
+    .catch(error => res.status(500).send(error.message));*/
 });
 
 const port = process.env.PORT || 3000;
